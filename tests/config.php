09dee0c6efb103b677b971eccc5a9e9e7801a6c2
MichaelBengtsson <michael@krokedil.se>
2020-04-23 12:06:03 +0200

    Added plugin file and config

---
+++ tests/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin configuration file.
+ *
+ * @author Krokedil
+ * @since 1.0.0
+ */
+return array(
+	'name'    => 'travis-test.php',
+	'version' => '1.0.0',
+);
---
+++ travis-test.php
@@ -0,0 +1,33 @@
+<?php // phpcs:ignore
+/**
+ * Plugin Name: Travis test
+ * Plugin URI: https://krokedil.com/
+ * Description: Just testing
+ * Author: Krokedil
+ * Author URI: https://krokedil.com/
+ * Version: 1.0.0
+ * Text Domain: just-testing
+ * Domain Path: /languages
+ *
+ * WC requires at least: 3.2.0
+ * WC tested up to: 3.8.1
+ *
+ * Copyright (c) 2017-2020 Krokedil
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Travis-Test
+ */
+
+ echo 'Hello World!';
