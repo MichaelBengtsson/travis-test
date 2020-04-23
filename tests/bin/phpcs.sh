7bb74520265c48f3a98e8aca822cf520d933787c
MichaelBengtsson <michael@krokedil.se>
2020-04-23 13:03:05 +0200

    testing cs

--- tests/bin/phpcs.sh
+++ tests/bin/phpcs.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-
+echo "Maybe code sniffer"
+echo ${RUN_PHPCS}
 if [[ ${RUN_PHPCS} == 1 ]]; then
+	echo "in if cs"
 	CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $TRAVIS_COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
 	IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
 
 	if [ "$CHANGED_FILES" != "" ]; then
+		echo "in if 2 cs"
 		# Install wpcs globally:
     	composer require woocommerce/woocommerce-sniffs
 
