9cc6dea5971f005ef4e9041fefe112be32f093cd
MichaelBengtsson <michael@krokedil.se>
2020-04-23 13:29:54 +0200

    Update phpcs.sh

--- tests/bin/phpcs.sh
+++ tests/bin/phpcs.sh
@@ -2,12 +2,12 @@
 echo "Maybe code sniffer"
 echo ${RUN_PHPCS}
 echo $TRAVIS_COMMIT_RANGE
-echo $IGNORE
-echo $CHANGED_FILES
 if [[ ${RUN_PHPCS} == 1 ]]; then
 	echo "in if cs"
 	CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $TRAVIS_COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
 	IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
+	echo $CHANGED_FILES
+	echo $IGNORE
 
 	if [ "$CHANGED_FILES" != "" ]; then
 		echo "in if 2 cs"
@@ -15,7 +15,6 @@ if [[ ${RUN_PHPCS} == 1 ]]; then
     	composer require woocommerce/woocommerce-sniffs
 
 		echo "Running Code Sniffer."
-		ls
-		./vendor/bin/phpcs phpcbf . --standard=WordPress --extensions=php --ignore=*/tests/*
+		./vendor/bin/phpcs --standard=WordPress --extensions=php --ignore=$IGNORE $CHANGED_FILES
 	fi
 fi
