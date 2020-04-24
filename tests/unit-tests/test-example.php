4a1f19ad074f6cf17f8060f4a5816709e2ac059d
MichaelBengtsson <michael@krokedil.se>
2020-04-24 08:55:32 +0200

    Update test-example.php

--- tests/unit-tests/test-example.php
+++ tests/unit-tests/test-example.php
@@ -3,6 +3,5 @@ class Example_Tests extends Krokedil_Unit_Test_Case implements KrokedilConstants
 	public function test_get_mapped_array() {
 			$this->assertSame( 'test', 'test' );
 			$this->assertTrue( true );
-			$this->assertTrue( false );
 	}
 }
