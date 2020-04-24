e60276c741cc1c3793f2fc05ecab19851a71136c
Michael Bengtsson <michael@krokedil.se>
2020-04-24 23:54:44 +0200

    again

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ ${RUN_PHPCS} == 0 ]]; then
 	git clone https://github.com/woocommerce/woocommerce.git '../woocommerce'
-	/home/travis/build/MichaelBengtsson/woocommerce/composer install
+	composer install -d=/home/travis/build/MichaelBengtsson/woocommerce/
 	$HOME/.composer/vendor/bin/phpunit -c phpunit.xml $@
 fi
