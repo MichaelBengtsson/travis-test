5e70c34ec78b62961d20680bf594788c6d2d32f2
Michael Bengtsson <michael@krokedil.se>
2020-04-24 18:04:52 +0200

    Testing trabis after docker stuff

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 if [[ ${RUN_PHPCS} == 0 ]]; then
 	git clone https://github.com/woocommerce/woocommerce.git '../woocommerce'
 	$HOME/.composer/vendor/bin/phpunit -c phpunit.xml $@
