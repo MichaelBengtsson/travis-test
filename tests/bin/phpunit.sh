fc837df833a22e1d75d13179d02fd732b74db966
Michael Bengtsson <michael@krokedil.se>
2020-04-24 23:51:42 +0200

    maybe fix wc stuff

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 if [[ ${RUN_PHPCS} == 0 ]]; then
 	git clone https://github.com/woocommerce/woocommerce.git '../woocommerce'
+	/home/travis/build/MichaelBengtsson/woocommerce/composer install
 	$HOME/.composer/vendor/bin/phpunit -c phpunit.xml $@
 fi
