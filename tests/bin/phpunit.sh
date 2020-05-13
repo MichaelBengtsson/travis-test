3638f91271d41ade5541ab6a9587f3e1b1fd53b3
Michael Bengtsson <michael@krokedil.se>
2020-05-13 19:47:23 +0200

    test

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-if [[ ${RUN_PHPCS} == 0 ]]; then
-	git clone --depth 1 --branch ${WC_VERSION} https://github.com/woocommerce/woocommerce.git '../woocommerce'
-	$HOME/.composer/vendor/bin/phpunit -c phpunit.xml $@
-fi
+git clone --depth 1 --branch 4.1.0 https://github.com/woocommerce/woocommerce.git '../woocommerce'
